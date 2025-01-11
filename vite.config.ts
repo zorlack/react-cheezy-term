d661c74ae657e97179e18c21eb8e10cce4c08fc8
Dave Seltzer <daveseltzer@gmail.com>
2025-01-11 04:53:10 +0000

    This thing builds now... so progress

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve('src/index.ts'),
+      name: 'ReactCheezyTerm',
+      fileName: (format) => `react-cheezy-term.${format}.js`,
+    },
+    rollupOptions: {
+      // Externalize dependencies to avoid bundling them
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
