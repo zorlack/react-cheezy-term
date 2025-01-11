a63423d0a67987e117e1785641e7ecc2a3e3e097
Dave Seltzer <daveseltzer@gmail.com>
2025-01-11 05:25:33 +0000

    testing asset integration

--- src/CheezyTerminal.tsx
+++ src/CheezyTerminal.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import testImage from './assets/images/rectoset.png';
 
 interface CheezyTerminalProps {
   title?: string;
@@ -6,7 +7,7 @@ interface CheezyTerminalProps {
 
 const CheezyTerminal: React.FC<CheezyTerminalProps> = ({ title = 'Cheezy Terminal' }) => {
   return (
-    <div style={{ fontFamily: 'monospace', padding: '1rem', border: '1px solid black' }}>
+    <div style={{ fontFamily: 'monospace', padding: '1rem', border: '1px solid black', backgroundImage: `url(${testImage})` }}>
       <h1>{title}</h1>
       <p>Welcome to the Cheezy Terminal!</p>
     </div>
