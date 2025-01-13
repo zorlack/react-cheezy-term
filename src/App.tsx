5598ad27b190b2591f06707bdb67a5c8766300da
zorlack <daveseltzer@gmail.com>
2025-01-12 19:30:51 -0500

    added more.

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useRef, useEffect } from 'react';
+import { ReactCheezyTerm, ReactCheezyTermRef } from './ReactCheezyTerm';
+
+const config = {
+  imagePath: 'react-cheezy-term/rectoset.png',
+  wrapperWidth: 1216,
+  wrapperHeight: 832,
+  startX: 283,
+  startY: 200,
+  consoleWidth: 463,
+  consoleHeight: 370,
+  xtermTheme: {
+    background: '#000000',
+    foreground: '#0aff0a',
+    cursor: '#00ff00'
+  }
+};
+
+export default function App() {
+  // Create a ref to the ReactCheezyTerm
+  const cheezyRef = useRef<ReactCheezyTermRef>(null);
+
+  useEffect(() => {
+    // When the component has mounted, we can access the Terminal
+    const terminal = cheezyRef.current?.getTerminal();
+    if (terminal) {
+      // Write something silly to the terminal
+      terminal.write('Welcome to the Cheezy Terminal!\r\n');
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Cheezy Terminal Example</h1>
+      <ReactCheezyTerm ref={cheezyRef} terminalConfig={config} />
+    </div>
+  );
+}
