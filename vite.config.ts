f8e16f5803ba07329e59862c04682fdbf43e7672
Dave Seltzer <daveseltzer@gmail.com>
2025-01-14 21:46:48 +0000

    Improve packaging... maybe

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      name: 'react-cheezy-term',
+      entry: ['src/main-lib.ts'],
+      fileName: (format, entryName) => `react-cheezy-term-${entryName}.${format}.js`,
+      cssFileName: 'react-cheezy-term-style',
+    },
+  },
 })
